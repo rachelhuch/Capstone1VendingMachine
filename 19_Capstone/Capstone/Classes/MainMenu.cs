1938ec027ed99ebc5db038c5d80780341ac9fe2e
Andrew Burkett <andrewburkett.abb@gmail.com>
2020-02-07 12:53:05 -0500

    update agian

--- 19_Capstone/Capstone/Classes/MainMenu.cs
+++ 19_Capstone/Capstone/Classes/MainMenu.cs
@@ -47,96 +47,99 @@ namespace Capstone.Classes
 
                     else if (choice == "2")
                     {
-
-                        Console.WriteLine("Purchase Menu");
-                        Console.WriteLine("1)Feed Money");
-                        Console.WriteLine("2) Select Product");
-                        Console.WriteLine("3) Finish Transaction");
-                        Console.Write("Enter your choice (1, 2, 3) here: ");
-
-                        string selection = Console.ReadLine();
-
-                        if (!(choice == "3" || choice == "2" || choice == "1"))
+                        int menu2 = 1;
+                        while (menu2 == 1)
                         {
-                            Console.WriteLine("Please choose again.");
-                        }
+                            Console.WriteLine("Purchase Menu");
+                            Console.WriteLine("1)Feed Money");
+                            Console.WriteLine("2) Select Product");
+                            Console.WriteLine("3) Finish Transaction");
+                            Console.Write("Enter your choice (1, 2, 3) here: ");
 
-                        else
-                        {
-                            if (selection == "1")
+                            string selection = Console.ReadLine();
+
+                            if (!(choice == "3" || choice == "2" || choice == "1"))
                             {
-                                Console.Write("How much are you depositing?\t");
-
-                                decimal moneyIn = decimal.Parse(Console.ReadLine());
-
-                                if (!(moneyIn % 1 == 0))
-                                {
-                                    Console.WriteLine("Not valid. Please enter whole dollar amounts");
-                                }
-                                else
-                                {
-                                    vm.FeedMoney(moneyIn);
-                                }
-
-
-
-
-                                Console.WriteLine($"Your balance is {vm.Balance:C}");
+                                Console.WriteLine("Please choose again.");
                             }
-                            else if (selection == "2")
-                            {
-                                // select product (item.key)
-                                Console.Write("What is your product?\t ex (A1)\t");
-                                string keyNumber = Console.ReadLine();
-                                keyNumber = keyNumber.ToUpper();
-                                if (!(vm.inventory.ContainsKey(keyNumber)))
-                                {
-                                    Console.WriteLine("Invalid entry. Please enter your product. ex. A1");
-                                }
-                                else
-                                {
-                                    if (vm.Balance < vm.inventory[keyNumber].Price)
-                                    {
-                                        Console.WriteLine("Please add more money.");
-                                    }
 
-                                    else
-                                    {
-                                        //decroment balance by item.value.price
-                                        vm.Balance -= vm.inventory[keyNumber].Price;
-
-                                        //decroment item.value.quanity by 1
-                                        vm.inventory[keyNumber].Price -= 1;
-
-                                        //some fancy CW 
-                                        Console.WriteLine($"Enjoy your {vm.inventory[keyNumber].ItemName}");
-
-                                        //chomp chop stuff
-                                        if (vm.inventory[keyNumber].ItemCategory == "Chip")
-                                        {
-                                            Console.WriteLine("\t Crunch Crunch, Yum!");
-                                        }
-                                        if (vm.inventory[keyNumber].ItemCategory == "Candy")
-                                        {
-                                            Console.WriteLine("\t Munch Munch, Yum!");
-                                        }
-                                        if (vm.inventory[keyNumber].ItemCategory == "Drink")
-                                        {
-                                            Console.WriteLine("\t Glug Glug, Yum!");
-                                        }
-                                        if (vm.inventory[keyNumber].ItemCategory == "Gum")
-                                        {
-                                            Console.WriteLine("\t Chew Chew, Yum!");
-                                        }
-                                        Console.WriteLine($"Your balance is {vm.Balance:C}");
-                                    }
-                                }
-                            }
                             else
                             {
-                                Console.WriteLine($"Your change is {vm.Balance}");
-                                vm.EndTransaction();
+                                if (selection == "1")
+                                {
+                                    Console.Write("How much are you depositing?\t");
 
+                                    decimal moneyIn = decimal.Parse(Console.ReadLine());
+
+                                    if (!(moneyIn % 1 == 0))
+                                    {
+                                        Console.WriteLine("Not valid. Please enter whole dollar amounts");
+                                    }
+                                    else
+                                    {
+                                        vm.FeedMoney(moneyIn);
+                                    }
+
+
+
+
+                                    Console.WriteLine($"Your balance is {vm.Balance:C}");
+                                }
+                                else if (selection == "2")
+                                {
+                                    // select product (item.key)
+                                    Console.Write("What is your product?\t ex (A1)\t");
+                                    string keyNumber = Console.ReadLine();
+                                    keyNumber = keyNumber.ToUpper();
+                                    if (!(vm.inventory.ContainsKey(keyNumber)))
+                                    {
+                                        Console.WriteLine("Invalid entry. Please enter your product. ex. A1");
+                                    }
+                                    else
+                                    {
+                                        if (vm.Balance < vm.inventory[keyNumber].Price)
+                                        {
+                                            Console.WriteLine("Please add more money.");
+                                        }
+
+                                        else
+                                        {
+                                            //decroment balance by item.value.price
+                                            vm.Balance -= vm.inventory[keyNumber].Price;
+
+                                            //decroment item.value.quanity by 1
+                                            vm.inventory[keyNumber].Price -= 1;
+
+                                            //some fancy CW 
+                                            Console.WriteLine($"Enjoy your {vm.inventory[keyNumber].ItemName}");
+
+                                            //chomp chop stuff
+                                            if (vm.inventory[keyNumber].ItemCategory == "Chip")
+                                            {
+                                                Console.WriteLine("\t Crunch Crunch, Yum!");
+                                            }
+                                            if (vm.inventory[keyNumber].ItemCategory == "Candy")
+                                            {
+                                                Console.WriteLine("\t Munch Munch, Yum!");
+                                            }
+                                            if (vm.inventory[keyNumber].ItemCategory == "Drink")
+                                            {
+                                                Console.WriteLine("\t Glug Glug, Yum!");
+                                            }
+                                            if (vm.inventory[keyNumber].ItemCategory == "Gum")
+                                            {
+                                                Console.WriteLine("\t Chew Chew, Yum!");
+                                            }
+                                            Console.WriteLine($"Your balance is {vm.Balance:C}");
+                                        }
+                                    }
+                                }
+                                else
+                                {
+                                    Console.WriteLine($"Your change is {vm.Balance}");
+                                    vm.EndTransaction();
+                                    menu2 = 2;
+                                }
                             }
                         }
                     }
